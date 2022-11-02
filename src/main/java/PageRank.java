d972c4d3ba1636b786d06dfc43650e497d543324
silas-wunder <55813205+silas-wunder@users.noreply.github.com>
2022-11-02 16:48:36 -0600

    added spark skeleton code

--- src/main/java/PageRank.java
+++ src/main/java/PageRank.java
@@ -1,7 +1,13 @@
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.SparkConf;
+
 public class PageRank {
 
     public static void main(String[] args) throws Exception {
+        SparkConf conf = new SparkConf().setAppName("Page Rank").setMaster("yarn");
+        JavaSparkContext sc = new JavaSparkContext(conf);
 
+        sc.close();
     }
 
 }
