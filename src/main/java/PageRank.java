468faf5c93da78c330eafdff6f613c6cc328380f
Silas Wunder <55813205+silas-wunder@users.noreply.github.com>
2022-11-06 00:58:38 -0600

    redid skeleton code

--- src/main/java/PageRank.java
+++ src/main/java/PageRank.java
@@ -1,13 +1,16 @@
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.SparkConf;
+import scala.Tuple2;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.sql.SparkSession;
 
 public class PageRank {
 
     public static void main(String[] args) throws Exception {
-        SparkConf conf = new SparkConf().setAppName("Page Rank").setMaster("yarn");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-
-        sc.close();
+        SparkSession spark = SparkSession.builder().appName("PageRank").getOrCreate();
+        JavaRDD<String> links = spark.read().textFile(args[0]).javaRDD();
+        JavaRDD<String> titles = spark.read().textFile(args[1]).javaRDD();
     }
 
 }
