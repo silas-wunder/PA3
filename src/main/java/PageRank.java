05f7c5317d6b5622fc83815b98b7a5fc9248e836
silas-wunder <55813205+silas-wunder@users.noreply.github.com>
2022-11-07 13:39:25 -0700

    added correct link and title input

--- src/main/java/PageRank.java
+++ src/main/java/PageRank.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.SparkSession;
+
+import scala.Tuple2;
+
 import org.apache.spark.mllib.linalg.distributed.CoordinateMatrix;
 import org.apache.spark.mllib.linalg.distributed.MatrixEntry;
 import org.apache.spark.mllib.linalg.distributed.IndexedRowMatrix;
@@ -10,10 +15,14 @@ public class PageRank {
     public static void main(String[] args) throws Exception {
         SparkSession spark = SparkSession.builder().appName("PageRank").getOrCreate();
         JavaRDD<String> linksRaw = spark.read().textFile(args[0]).javaRDD();
-        JavaRDD<String> titles = spark.read().textFile(args[1]).javaRDD();
-
-        // TODO: write mapping function to get link representation right
-        JavaPairRDD<String, Iterable<String>> links = linksRaw.mapToPair(null);
+        JavaPairRDD<Integer, Iterable<Integer>> links = linksRaw.mapToPair(s -> {
+            String[] parts = s.split(": ");
+            Iterable<Integer> tos = Arrays.asList(
+                    Arrays.stream(parts[1].split(" ")).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new));
+            return new Tuple2<Integer, Iterable<Integer>>(Integer.parseInt(parts[0]), tos);
+        });
+        JavaPairRDD<Long, String> titles = spark.read().textFile(args[1]).javaRDD().zipWithIndex()
+                .mapToPair(x -> new Tuple2<>(x._2(), x._1())).cache();
     }
 
 }
