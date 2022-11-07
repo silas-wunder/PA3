a16b12790596a0d5e5f18056a29a9597002dddb4
silas-wunder <55813205+silas-wunder@users.noreply.github.com>
2022-11-07 12:48:52 -0700

    began work on converting links

--- src/main/java/PageRank.java
+++ src/main/java/PageRank.java
@@ -1,16 +1,19 @@
-import scala.Tuple2;
-
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.mllib.linalg.distributed.CoordinateMatrix;
+import org.apache.spark.mllib.linalg.distributed.MatrixEntry;
+import org.apache.spark.mllib.linalg.distributed.IndexedRowMatrix;
 
 public class PageRank {
 
     public static void main(String[] args) throws Exception {
         SparkSession spark = SparkSession.builder().appName("PageRank").getOrCreate();
-        JavaRDD<String> links = spark.read().textFile(args[0]).javaRDD();
+        JavaRDD<String> linksRaw = spark.read().textFile(args[0]).javaRDD();
         JavaRDD<String> titles = spark.read().textFile(args[1]).javaRDD();
+
+        // TODO: write mapping function to get link representation right
+        JavaPairRDD<String, Iterable<String>> links = linksRaw.mapToPair(null);
     }
 
 }
