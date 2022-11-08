2dc564101852f4fac59408aabf0ca30dd9f5ef26
silas-wunder <55813205+silas-wunder@users.noreply.github.com>
2022-11-08 14:00:44 -0700

    fninished matrix multiplication and final write

--- src/main/java/PageRank.java
+++ src/main/java/PageRank.java
@@ -1,37 +1,50 @@
 import java.util.Arrays;
 import java.util.ArrayList;
 
-import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
 
 import scala.Tuple2;
 
+import org.apache.spark.mllib.linalg.DenseMatrix;
 import org.apache.spark.mllib.linalg.distributed.CoordinateMatrix;
 import org.apache.spark.mllib.linalg.distributed.MatrixEntry;
 import org.apache.spark.rdd.RDD;
-import org.apache.spark.mllib.linalg.distributed.IndexedRowMatrix;
 
 public class PageRank {
 
     public static void main(String[] args) throws Exception {
         SparkSession spark = SparkSession.builder().appName("PageRank").getOrCreate();
-        JavaSparkContext sc = new JavaSparkContext();
+        SparkContext context = spark.sparkContext();
+        JavaSparkContext sc = JavaSparkContext.fromSparkContext(context);
         JavaRDD<String> linksRaw = spark.read().textFile(args[0]).javaRDD();
         JavaPairRDD<Long, long[]> links = linksRaw
                 .mapToPair(s -> new Tuple2<Long, long[]>(Long.parseLong(s.split(": ")[0]),
                         Arrays.stream(s.split(": ")[1].split(" ")).mapToLong(Long::parseLong).toArray()));
         JavaPairRDD<Long, String> titles = spark.read().textFile(args[1]).javaRDD().zipWithIndex()
                 .mapToPair(x -> new Tuple2<>(x._2(), x._1())).cache();
-        // TODO: I'm pretty sure this is wrong, should be changed
-        RDD<MatrixEntry> entries = links.flatMap(l -> {
+        // TODO: maybe not wrong? someone should take a look at this tho
+        RDD<MatrixEntry> linkEntries = links.flatMap(link -> {
             ArrayList<MatrixEntry> es = new ArrayList<MatrixEntry>();
-            for (long i : l._2()) {
-                es.add(new MatrixEntry(l._1(), i, 1.0 / l._2().length));
+            for (long i : link._2()) {
+                es.add(new MatrixEntry(link._1(), i, 1.0 / link._2().length));
             }
             return es.iterator();
         }).rdd();
-        CoordinateMatrix linkMatrix = new CoordinateMatrix(entries, titles.count(), titles.count());
+        CoordinateMatrix linkMatrix = new CoordinateMatrix(linkEntries, titles.count(), titles.count());
+        JavaRDD<Double> ranks = links.map(link -> 1.0 / titles.count());
+        DenseMatrix rankVector = new DenseMatrix((int) titles.count(), 1,
+                ArrayUtils.toPrimitive((Double[]) ranks.collect().toArray()));
+        for (int i = 0; i < 25; i++) {
+            rankVector = linkMatrix.toBlockMatrix().toLocalMatrix().multiply(rankVector);
+        }
+        JavaPairRDD<Long, Double> ranksSorted = sc.parallelize(Arrays.asList(ArrayUtils.toObject(rankVector.values())))
+                .zipWithIndex().sortByKey().mapToPair(x -> new Tuple2<>(x._2(), x._1()));
+        JavaRDD<Tuple2<Double, String>> ranksTitledSorted = ranksSorted.join(titles).values();
+        ranksTitledSorted.saveAsTextFile(args[2]);
     }
 }
