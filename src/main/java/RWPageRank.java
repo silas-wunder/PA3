cee2f3a372d2d5447a94d7752e58d213da2b40e8
Abii Franke <77130061+Kouvei@users.noreply.github.com>
2022-11-09 18:23:42 -0700

    Update RWPageRank.java

--- src/main/java/RWPageRank.java
+++ src/main/java/RWPageRank.java
@@ -15,4 +15,16 @@ import org.apache.spark.mllib.linalg.SparseMatrix;
 import org.apache.spark.mllib.linalg.distributed.MatrixEntry;
 
 public class RWPageRank {
+  public static void main(String[] args) throws Exception {
+        SparkSession spark = SparkSession.builder().appName("PageRank").getOrCreate();
+        SparkContext context = spark.sparkContext();
+        JavaSparkContext sc = JavaSparkContext.fromSparkContext(context);
+        JavaRDD<String> linksRaw = spark.read().textFile(args[0]).javaRDD();
+        JavaPairRDD<Long, long[]> links = linksRaw
+                .mapToPair(s -> new Tuple2<Long, long[]>(Long.parseLong(s.split(": ")[0]),
+                        Arrays.stream(s.split(": ")[1].split(" ")).mapToLong(Long::parseLong).toArray()));
+        JavaPairRDD<Long, String> titles = spark.read().textFile(args[1]).javaRDD().zipWithIndex()
+                .mapToPair(x -> new Tuple2<>(x._2(), x._1())).cache();
+        long numPages = titles.count();
+    }
 }
