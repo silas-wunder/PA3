68b0dd3795221b4a0141845ebdfba743385306f5
Abii Franke <77130061+Kouvei@users.noreply.github.com>
2022-11-10 14:02:24 -0700

    Create RMBomb.java

---
+++ src/main/java/RMBomb.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.SparkSession;
+
+import scala.Tuple2;
+
+public class RMBomb {
+    
+}
